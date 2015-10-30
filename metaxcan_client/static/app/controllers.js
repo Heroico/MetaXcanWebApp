f69f085ac24b64632663565ea58b0e535117874b
Numa <alvarobarbeira@gmail.com>
2015-10-30 16:31:47 -0300

    Got the angular app loaded in the browser. HTML is still wrong, though

---
+++ metaxcan_client/static/app/controllers.js
@@ -0,0 +1,14 @@
+(function(){
+    'use strict';
+
+    /* Controllers */
+    angular.module('metaxcanClientControllers', [])
+        .controller('HomeCtrl', home)
+        .controller('LoginCtrl', login)
+        .controller('SignUpCtrl', signUp);
+
+    function home(){};
+    function login(){};
+    function signUp(){};
+
+})();
--- metaxcan_client/static/app/metaxcan_client.js
+++ metaxcan_client/static/app/metaxcan_client.js
@@ -1,2 +1,27 @@
-angular
-  .module('metaxcan_client', []);
+(function(){
+    angular
+        .module('metaxcan_client', ['metaxcanClientControllers', 'ngRoute'])
+        .config(['$routeProvider',configRouteProvider]);
+
+    function configRouteProvider($routeProvider) {
+        $routeProvider.
+            when('/home', {
+                templateUrl: 'static/app/partials/home.html',
+                controller: 'HomeCtrl'
+              }).
+            when('/login', {
+                templateUrl: 'static/app/partials/login.html',
+                controller: 'LoginCtrl',
+                controllerAs: 'vm',
+            }).
+            when('/register', {
+                templateUrl: 'static/app/partials/register.html',
+                controller: 'RegisterCtrl',
+                controllerAs: 'vm',
+            }).
+            otherwise({
+                redirectTo: '/home'
+            });
+  }
+
+})();

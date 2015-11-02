6839071ad5fbd8f1eb57845524d19dc1adb612c5
Numa <alvarobarbeira@gmail.com>
2015-11-02 19:08:22 -0300

    Added login (and refactored user ser service)

--- metaxcan_client/static/app/login_controller.js
+++ metaxcan_client/static/app/login_controller.js
@@ -3,11 +3,36 @@
 
     /* Controllers */
     angular.module('metaxcanClientControllers')
-        .controller('LoginCtrl', ["$scope", login]);
+        .controller('LoginCtrl', ["$scope", "$location", "userService", login]);
 
-    function login($scope){
+    function login($scope, $location, userService){
         var vm = this;
-        vm.user = {name:null, password:null, email:null }
+        vm.user = {name_or_email:null, password:null}
+        vm.message = null;
+        vm.login = login
+
+        function login(){
+            vm.message = null;
+
+            var name = null
+            var email = null
+/*          TODO: enable login with email
+            var re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+            if (re.test(vm.user.name_or_email)) {
+                email = vm.user.name_or_email
+            } else {
+                name = vm.user.name_or_email
+            }
+*/
+            name = vm.user.name_or_email
+
+            userService.login(name, email, vm.user.password, function(){
+                vm.message = "OK";
+                $location.path("home");
+            }, function(error){
+                vm.message = error;
+            })
+        }
     };
 
 })();
--- metaxcan_client/static/app/user_services.js
+++ metaxcan_client/static/app/user_services.js
@@ -6,6 +6,8 @@
         var service = {}
         service.token = null
         service.signup = signup
+        service.login = login
+        service.loggedin = loggedin
 
         return service;
 
@@ -13,40 +15,59 @@
             return service.token != null;
         }
 
-        function signup(name, email, password, success, error) {
-            var p = $http.post("/api/users/create/",
-                {
-                    username: name,
-                    email: email,
-                    password: password
-                }
+        function signup(name, email, password, success_handler, error_handler) {
+            user_request("/api/users/create/", name, email, password, success_handler, error_handler)
+        }
+
+        function login(name, email, password, success_handler, error_handler) {
+            user_request("/api/token/", name, email, password, success_handler, error_handler)
+        }
+
+        function user_request(url, name, email, password, success_handler, error_handler) {
+            var request = {password: password}
+            if (name)
+                request.username = name
+            if (email)
+                request.email = email
+            var p = $http.post(url, request
             ).then(function(response){
-                service.token = response.data.token
-                if (success)
-                    success()
+                process_success(response, success_handler)
             }, function(response){
-                var message = null
-
-                if (response != undefined &&
-                    typeof response == "object" &&
-                    typeof response.data == "object" ){
-                    data = response.data
-                    if ("username" in data) {
-                        message = "User is invalid (or already taken)"
-                    } else if ("password" in data) {
-                        message = data.password
-                    } else if ("email" in data) {
-                        message = data.email
-                    } else {
-                        message = "Something went wrong. "+response.status
-                    }
-                } else {
-                    message = "Something went very wrong. "+response.status
-                }
-
-                if (error)
-                    error(message)
+                process_error(response, error_handler)
             })
+        }
+
+        function process_success(response, success_handler) {
+            service.token = response.data.token
+            if (success_handler)
+                success_handler();
+        }
+
+        function process_error(response, error_handler) {
+            var message = null
+
+            if (response != undefined &&
+                typeof response == "object" &&
+                typeof response.data == "object" ){
+                data = response.data
+                if ("username" in data) {
+                    message = data.username//"User is invalid (or already taken)"
+                } else if ("password" in data) {
+                    message = data.password
+                } else if ("email" in data) {
+                    message = data.email
+                } else if ("non_field_errors" in data) {
+                    message = data.non_field_errors[0]
+                } else {
+                    message = "Something went wrong. "+response.status
+                }
+            } else {
+                message = "Something went very wrong. "+response.status
+            }
+
+            if (error_handler)
+                error_handler(message)
+
         }
     };
 })();
